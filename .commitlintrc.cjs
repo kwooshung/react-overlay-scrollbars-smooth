e784b83fb636fdd6da0422a8acc7e724aace7997
kwooshung <kwooshung@qq.com>
2024-02-05 23:16:47 +0800

    🐛 fix(lib): Change the Smoothscroll from CJS to ESM and save local locally

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,10 +1,1 @@
-const commitTypes = require('./tools/commitTypes.cjs');
-const commitScopes = require('./tools/commitScopes.cjs');
-
-module.exports = {
-  extends: ['git-commit-emoji', 'cz'],
-  rules: {
-    'type-enum': [2, 'always', commitTypes.map((type) => type.value)],
-    'scope-enum': [2, 'always', commitScopes.map((scope) => scope[0])]
-  }
-};
+module.exports = require('./configs/commitlintrc.cjs');
